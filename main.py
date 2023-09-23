3e25ffa5557647c2253660393e3757cf89e17081
SyniRon <66834451+SyniRon@users.noreply.github.com>
2023-09-23 08:09:41 -0400

    improve error handling and messaging

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import os
 import aiohttp
 import discord
 from discord import app_commands
+import logging
+import traceback
+
+if not all(os.getenv(var) for var in ["DISCORD_ID", "API_KEY", "OWNER_ID", "BOT_TOKEN"]):
+    logging.error("Missing essential environment variables.")
+    exit(1)
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 # Create Discord intents and enable message content
 intents = discord.Intents.all()
@@ -17,6 +25,10 @@ tree = app_commands.CommandTree(client)
 
 # Set the bot's status activity
 status = discord.Activity(name="https://7cav.us", type=3)
+
+discord_id = os.getenv("DISCORD_ID")
+owner_id = int(os.getenv("OWNER_ID"))
+
 
 # Define a dictionary mapping server names to their corresponding server IDs
 servers_dict = {
@@ -39,13 +51,14 @@ async def on_command_error(
 ) -> None:
     if isinstance(error, app_commands.errors.MissingAnyRole):
         # Respond with an error message if the user doesn't have the required role
+        logging.error(f"Missing role! Error: {error}")
         await interaction.response.send(
             f"You must have one of these roles: `{error.missing_roles}` in order to use this command",
             ephemeral=True,
         )
     else:
         # Forward the error message to the bot owner and respond with a generic error message
-        sypolt = await client.fetch_user(130158049968128000)
+        logging.error(f"Sypolt Send! Error: {error}")
         await sypolt.send(
             f"Someone broke your bot in a new and interesting way ```{error}```"
         )
@@ -60,8 +73,8 @@ Why don't you try whatever that was again but with less breaking things this tim
 # Command decorator for the restart_server command
 @tree.command(
     name="restart_server",
-    description="For SGT+ to restart Arma and Squad servers",
-    guild=discord.Object(id=109869242148491264),
+    description="For authorized persons to restart Arma and Squad servers",
+    guild=discord.Object(id=discord_id),
 )
 @app_commands.describe(servers="Servers to choose from")
 @app_commands.choices(
@@ -77,6 +90,8 @@ Why don't you try whatever that was again but with less breaking things this tim
 async def restart_server(interaction, servers: discord.app_commands.Choice[int]):
     # Defer the response to acknowledge the command
     await interaction.response.defer()
+    username = interaction.user.name
+    logging.info(f"{username} initiated the restart_server command.")
 
     # Get the server ID based on the chosen server name
     server_id = servers_dict[servers.name]
@@ -95,48 +110,55 @@ async def restart_server(interaction, servers: discord.app_commands.Choice[int])
     # Prepare payload to start the server process
     payload_start = '{"signal": "start"}'
 
-    # Send a request to stop the server
+    # Send a request to the server
     async with aiohttp.ClientSession() as session:
-        async with session.post(url, headers=headers, data=payload_kill) as response:
-            if response.status == 204:
-                # Wait for a few seconds to ensure the server is stopped
-                await asyncio.sleep(5)
+        try:
+            async with session.post(url, headers=headers, data=payload_kill) as response:
+                if response.status == 204:
+                    await asyncio.sleep(5)
 
-                # Send a request to start the server
-                async with aiohttp.ClientSession() as session:
-                    async with session.post(
-                        url, headers=headers, data=payload_start
-                    ) as response:
+                    async with session.post(url, headers=headers, data=payload_start) as response:
                         if response.status == 204:
-                            # Respond with a success message if the server restarted successfully
-                            await interaction.followup.send(
-                                f"Server {servers.name} restarted successfully!"
-                            )
+                            logging.info(f"Server {servers.name} restarted successfully!")
+                            await interaction.followup.send(f"✅ Server {servers.name} restarted successfully!")
+                            await sypolt.send(f"DEBUG: {username} used /restart_server and {servers.name} restarted successfully!")
                         else:
-                            # Respond with an error message if the server failed to start
-                            await interaction.followup.send(
-                                f"Server start failed on {servers.name}."
-                            )
-            else:
-                # Respond with an error message if the server failed to stop
-                await interaction.followup.send(
-                    f"Failed to stop server {servers.name}."
-                )
+                            logging.error(f"Server start failed on {servers.name}.")
+                            await interaction.followup.send(f"❌ Server start failed on {servers.name}.")
+                            await sypolt.send("DEBUG:{username} used /restart_server and server start failed on {servers.name}.")
+                else:
+                    logging.error(f"Failed to stop server {servers.name}.")
+                    await interaction.followup.send(f"❌ Failed to stop server {servers.name}.")
+                    await sypolt.send(f"DEBUG: {username} used /restart_server and failed to stop server {servers.name}.")
+        except aiohttp.ClientError as e:
+            logging.error(f"Network error: {e}")
+            await sypolt.send(f"DEBUG: {username} used /restart_server and network error: {e}")
+            await interaction.followup.send(f"❌ Network error: {e}")
+        except asyncio.TimeoutError:
+            logging.error("API request timed out.")
+            await sypolt.send(f"DEBUG: {username} used /restart_server and API request timed out.")
+            await interaction.followup.send("❌ API request timed out.")
+        except Exception as e:
+            logging.error(f"An unexpected error occurred: {e}")
+            await interaction.followup.send(f"❌ An unexpected error occurred: {e}")
+            error_info = f"Traceback: {traceback.format_exc()}, User: {interaction.user.id}"
+            await sypolt.send(f"An unexpected error occurred: {e}, {error_info}")
 
 
 # Event handler for when the bot is ready
 @client.event
 async def on_ready():
+    global sypolt
+    sypolt = await client.fetch_user(owner_id)
     # Set the bot's status activity
     await client.change_presence(activity=status)
 
     # Sync the command tree with the guild
-    await tree.sync(guild=discord.Object(id=109869242148491264))
+    await tree.sync(guild=discord.Object(id=discord_id))
 
     # Print a message to indicate the bot is ready
-    print("Bot ready")
+    logging.info("Bot ready.")
 
 
 # Retrieve the bot token from the environment variable and run the client
-token = client.run(os.getenv("BOT_TOKEN"))
-client.run(token)
+client.run(os.getenv("BOT_TOKEN"))
