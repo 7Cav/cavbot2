d6e379ec7a614c71dfb4a0d7a09e5560c6996c2f
SyniRon <66834451+SyniRon@users.noreply.github.com>
2025-01-06 01:23:00 -0500

    initial go version commit with some test commands fixes #8

---
+++ commands/registry.go
@@ -0,0 +1,39 @@
+package commands
+
+import "github.com/bwmarrin/discordgo"
+
+type Registry struct {
+	commands []Command
+}
+
+func NewRegistry() *Registry {
+	r := &Registry{}
+	r.RegisterCommands(
+		Ping(),
+		Echo(),
+		Image(),
+		Milpac(),
+	)
+	return r
+}
+
+func (r *Registry) RegisterCommands(cmds ...Command) {
+	r.commands = append(r.commands, cmds...)
+}
+
+func (r *Registry) GetCommands() []*discordgo.ApplicationCommand {
+	cmds := make([]*discordgo.ApplicationCommand, len(r.commands))
+	for i, cmd := range r.commands {
+		cmds[i] = cmd.Definition
+	}
+	return cmds
+}
+
+func (r *Registry) GetHandler(name string) (CommandHandler, bool) {
+	for _, cmd := range r.commands {
+		if cmd.Definition.Name == name {
+			return cmd.Handler, true
+		}
+	}
+	return nil, false
+}
