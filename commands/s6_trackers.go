82bc23edba25262828fe2672b4e3f504ecbec95d
SyniRon <66834451+SyniRon@users.noreply.github.com>
2025-01-31 20:47:20 -0500

    if it errors we want it to break

--- commands/s6_trackers.go
+++ commands/s6_trackers.go
@@ -208,20 +208,19 @@ func handleS6ITCheckCommand(s *discordgo.Session, i *discordgo.InteractionCreate
 
 	eligibleMembers := []ITMember{}
 	currentDate := time.Now()
-	var errs []error
 	var matches []string
 	for _, member := range s6Members.LiteProfiles {
 		fullProfile, err := utils.GetMilpacByKeycloakID(ctx, member.KeycloakID)
 		if err != nil {
-			errs = append(errs, fmt.Errorf("failed to fetch milpac for %s: %v", member.User.Username, err))
-			continue
+			utils.HandleError(s, i, fmt.Sprintf("❌ Failed to fetch milpac: %v", err))
+			return
 		}
 
 		if strings.Contains(fullProfile.Primary.PositionTitle, "IT") && strings.Contains(fullProfile.Primary.PositionTitle, "S6") {
 			position, timeInPosition, positionDate, err := determineITPositionTime(fullProfile.Primary.PositionTitle, fullProfile)
 			if err != nil {
-				errs = append(errs, fmt.Errorf("failed to determine time in position: %v", err))
-				continue
+				utils.HandleError(s, i, fmt.Sprintf("❌ Failed to determine time in position: %v", err))
+				return
 			}
 
 			if positionDate.Before(currentDate.AddDate(0, -6, 0)) {
@@ -244,8 +243,8 @@ func handleS6ITCheckCommand(s *discordgo.Session, i *discordgo.InteractionCreate
 			if strings.Contains(secondary.PositionTitle, "IT") && strings.Contains(secondary.PositionTitle, "S6") {
 				position, timeInPosition, positionDate, err := determineITPositionTime(secondary.PositionTitle, fullProfile)
 				if err != nil {
-					errs = append(errs, fmt.Errorf("failed to determine time in position: %v", err))
-					continue
+					utils.HandleError(s, i, fmt.Sprintf("❌ Failed to determine time in position: %v", err))
+					return
 				}
 				if positionDate.Before(currentDate.AddDate(0, -6, 0)) {
 					matches = regexp.MustCompile(`/\d+/(\d+)\.jpg`).FindStringSubmatch(member.UniformUrl)
