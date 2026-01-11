4f3216f10a2c893ece30e97e619a47339ed18fef
Jens Gryspeert <jens@salesnote.eu>
2026-01-11 15:48:19 +0100

    Register command warden

---
+++ commands/warden.go
@@ -0,0 +1,15 @@
+package commands
+
+import (
+    "fmt"
+    "strings"
+    "github.com/7cav/cavbot2/utils"
+    "github.com/bwmarrin/discordgo"
+)
+
+const ROLE_NAME string = "Warden Verified"
+const ROLE_NAME_ADMIN string = "Warden Admin"
+
+func Warden() Command {
+
+}
