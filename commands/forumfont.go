15c2c4c217a7e5c3f06abf4daa468daf390ffd97
SyniRon <66834451+SyniRon@users.noreply.github.com>
2026-03-31 19:30:31 -0400

    add forumfont command

---
+++ commands/forumfont.go
@@ -0,0 +1,39 @@
+package commands
+
+import (
+	"fmt"
+	"math/rand"
+	"github.com/7cav/cavbot2/utils"
+	"github.com/bwmarrin/discordgo"
+)
+
+func ForumFont() Command {
+	return Command{
+		Definition: &discordgo.ApplicationCommand{
+			Name:        "forumfont",
+			Description: "Submit an opt-out request for the new forum font",
+		},
+		Handler: func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			username := i.Member.User.Username
+			if i.Member.Nick != "" {
+				username = i.Member.Nick
+			}
+			utils.Info("Forum font opt-out requested", "command", "ForumFont", "username", i.Member.User.Username, "discord_id", i.Member.User.ID)
+			queuePos := rand.Intn(200) + 350
+			hours := rand.Intn(6) + 22
+			minutes := rand.Intn(59) + 1
+			err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: fmt.Sprintf("[CAVBOT]: Opt-out request for User [%s] received. Ticket #8821-B has been generated. Your current position in the processing queue: %d. Estimated time until font reversion: %d hours, %d minutes.", username, queuePos, hours, minutes),
+					Flags:   discordgo.MessageFlagsEphemeral,
+				},
+			})
+			if err != nil {
+				utils.HandleError(s, i, fmt.Sprintf("❌ Failed to respond to interaction: %v", err))
+				return
+			}
+			utils.Info("✨ Done!", "command", "ForumFont")
+		},
+	}
+}
