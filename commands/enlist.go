5f261256a6783517d79dc79655e6a9601eaf17ef
BasherJAD <justin.dement89@gmail.com>
2026-08-18 12:46:08 -0400

    Update enlist.go

--- commands/enlist.go
+++ commands/enlist.go
@@ -1,8 +1,9 @@
 package commands
 
 import (
-	"github.com/bwmarrin/discordgo"
+
 	"fmt"
+	"github.com/bwmarrin/discordgo"
 	"github.com/7cav/cavbot2/utils"
 )
 
@@ -15,10 +16,14 @@ func Enlist() Command {
 		Handler: handleEnlistCommand,
 	}
 }
-
 func handleEnlistCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	runEnlist(utils.NewSessionResponder(s), i)
+}
+
+func runEnlist(r utils.InteractionResponder, i *discordgo.InteractionCreate) {
 	const enlistImageURL = "https://wiki.7cav.us/images/3/38/Cav_Enlistment_Infographic_1000_2000px_1_1.png"
-	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+
+	err := r.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
 			Embeds: []*discordgo.MessageEmbed{
@@ -34,8 +39,8 @@ func handleEnlistCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	})
 
 	if err != nil {
-    utils.Error("❌ Interaction response failed", "error", err)
-    utils.HandleError(s, i, fmt.Sprintf("❌ Failed to respond to interaction: %v", err))
-    return
+		utils.Error("❌ Interaction response failed", "error", err)
+		utils.HandleError(r, i, fmt.Sprintf("❌ Failed to respond to interaction: %v", err))
+		return
 	}
 }
