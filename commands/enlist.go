528465367a393f77ee54321f03eca4dcd97f884e
BasherJAD <justin.dement89@gmail.com>
2026-08-18 19:29:29 -0400

    Add enlist command (#227)
    * Create enlist_test.go
    
    * Rename enlist_test.go to enlist.go
    
    * Update enlist.go
    
    * Update registry.go
    
    * Update enlist.go
    
    * Update enlist.go
    
    * Update enlist.go
    
    * Update enlist.go
    
    added utils
    
    * Update commands/enlist.go
    
    Co-authored-by: JensGryspeert <jens.gryspeert@outlook.com>
    
    * Update enlist.go
    
    * Update enlist.go
    
    ---------
    
    Co-authored-by: JensGryspeert <jens.gryspeert@outlook.com>

---
+++ commands/enlist.go
@@ -0,0 +1,46 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/7cav/cavbot2/utils"
+	"github.com/bwmarrin/discordgo"
+)
+
+func Enlist() Command {
+	return Command{
+		Definition: &discordgo.ApplicationCommand{
+			Name:        "enlist",
+			Description: "Show the Enlistment Process",
+		},
+		Handler: handleEnlistCommand,
+	}
+}
+
+func handleEnlistCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	runEnlist(utils.NewSessionResponder(s), i)
+}
+
+func runEnlist(r utils.InteractionResponder, i *discordgo.InteractionCreate) {
+	const enlistImageURL = "https://wiki.7cav.us/images/3/38/Cav_Enlistment_Infographic_1000_2000px_1_1.png"
+
+	err := r.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{
+				{
+					Title:       "Enlistment Process",
+					Description: "Follow the steps below to enlist.",
+					Image: &discordgo.MessageEmbedImage{
+						URL: enlistImageURL,
+					},
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		utils.Error("❌ Interaction response failed", "error", err)
+		utils.HandleError(r, i, fmt.Sprintf("❌ Failed to respond to interaction: %v", err))
+		return
+	}
+}
